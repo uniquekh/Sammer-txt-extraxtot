fef6b59a1d54fa0e404545e5a445668174be72d6
Unique <mujeem000@gmail.com>
2024-06-27 18:39:24 +0530

    Update and rename config(1).py to config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6710743479:AAEOl7KxJAUdzkuDi7q5BK0d_f7La98_X2U")
     API_ID = int(os.environ["API_ID", 21179966]
     API_HASH = os.environ["API_HASH", "d97919fb0a3c725e8bb2a25bbb37d57c"]
     AUTH_USERS = "7326397503"""
